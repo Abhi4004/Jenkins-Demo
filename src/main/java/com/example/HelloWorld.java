37fc9f015ea2a070fea71c33116777c295c8c42d
Abhiram <268.reddabhiram@gmail.com>
2025-08-04 03:56:35 -0500

    New Java Test Code

---
+++ src/main/java/com/example/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.example;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello from Jenkins using Maven 3.9.9!");
+    }
+
+    public static String greet(String name) {
+        return "Hello, " + name + "!";
+    }
+}
---
+++ src/test/java/com/example/HelloWorldTest.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class HelloWorldTest {
+
+    @Test
+    public void testGreet() {
+        assertEquals("Hello, Jenkins!", HelloWorld.greet("Jenkins"));
+    }
+}
